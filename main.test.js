4618a42f537d897db78b3faa5f73bf60cad4a1e4
goni715 <halesgoni715149123@gmail.com>
2026-07-05 08:04:35 +0600

    write multiple test for one function

--- main.test.js
+++ main.test.js
@@ -1,8 +1,37 @@
-import { it, expect } from "vitest";
+import { it, expect, test } from "vitest";
 import { add } from "./main";
 
 it("Should return the correct sum if an array of number is provided", () => {
-  const result = add([1, 2, 3]);
-  
-  expect(result).toBe(6);
+  //Arrange
+  const numbers = [1, 2, 3];
+  const expectedResult = numbers.reduce((acc, cv) => acc + cv);
+
+  //Action
+  const result = add(numbers);
+
+  //Assertion
+  expect(result).toBe(expectedResult);
+});
+
+it("It Should return NaN if at least one invalid number is provided", () => {
+  //Arrange
+  const numbers = [1, "gg", 3];
+
+  //Action
+  const result = add(numbers);
+
+  //Assertion
+  expect(result).toBeNaN();
+});
+
+test("It Should return correct sum if an array of numeric string is provided", () => {
+  //Arrange
+  const numbers = ["1", "2", "3"];
+  const expectedResult = numbers.reduce((acc, cv) => Number(acc) + Number(cv));
+
+  //Action
+  const result = add(numbers);
+
+  //Assertion
+  expect(result).toBe(expectedResult);
 });
